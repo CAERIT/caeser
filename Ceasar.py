2d835c27dc61fa8ed237fb1378700d9674a4530c
calvin eisenhofer <cae5899@rit.edu>
2026-09-23 18:22:19 -0400

    decrypt loop

--- Ceasar.py
+++ Ceasar.py
@@ -4,9 +4,18 @@ def shift(data,key):
     while key < 0:
         key += 26
     while key >=26:
-        key -=26
+        key -= 26
+
     for char in data:
-        phrase += chr(ord(char)+key)
+        if ord(char) ==32:
+            phrase += char
+            continue
+        newchar = ord(char)+key
+        if ord(char) <=90 and newchar >90:
+            newchar -=26
+        if ord(char) >96 and newchar > 122:
+            newchar -=26
+        phrase += chr(newchar)
     return phrase
 
 def decryptLoop(data):
@@ -22,7 +31,9 @@ def main():
         key = int(input("enter your key: "))
         print("resulting output: ", shift(message,key))
     elif choice =="d":
-        print(decryptLoop(message))
+        results = decryptLoop(message)
+        for key in results:
+            print(key,"(",26-key,"):",results[key])
     else:
         print("select e or d")
 
